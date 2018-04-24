398ffd402fcbb40f923ca315d08c05b6e068b19c
Ronan Sales Lima <ronan.lima@fornecedores.sicoob.com.br>
2018-04-24 16:59:25 -0300

    Criando projeto Android.

---
+++ app/src/main/java/com/br/vitrineescritores/MainActivity.java
@@ -0,0 +1,18 @@
+package com.br.vitrineescritores;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Ronan.lima on 24/04/2018.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
